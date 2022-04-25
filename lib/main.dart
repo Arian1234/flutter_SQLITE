001c4ab5aed7ae59c7a31d1d470b8ce8b84f40f8
Alvaro Molina Cruz <amolina5678@hotmail.com>
2022-04-25 16:07:16 -0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/pages/home_page.dart';
+import 'package:flutter_sqlite/pages/mapa_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      initialRoute: 'Home',
+      routes: {
+        'Home': (BuildContext context) => const Homepage(),
+        'Mapa': (BuildContext context) => const Mapapage(),
+      },
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter SQLITE'),
+          title: const Text('Flutter SQLITE'),
         ),
         body: Center(
           child: Container(
-            child: Text('Hello World'),
+            child: const Text('Hello World'),
           ),
         ),
       ),
